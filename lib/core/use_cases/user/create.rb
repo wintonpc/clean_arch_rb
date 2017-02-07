ab258fb4976a546abb3264fec0f3d69d52c9abf2
Peter Winton <pwinton@indigobio.com>
2017-02-07 16:51:41 -0500

    structures instead of handlers

--- lib/core/use_cases/user/create.rb
+++ lib/core/use_cases/user/create.rb
@@ -4,15 +4,29 @@ module Core
   module UseCases
     module User
       class Create
-        def self.execute(username:, handler:, repo:)
+        Success = Struct.new(:created_user_id)
+        Failure = Struct.new(:validation_errors)
+
+        def self.execute(username:, repo:)
           if username.empty?
-            handler.validation_failed(username: :required)
+            validation_failed(username: :required)
           elsif repo.find_by_username(username)
-            handler.validation_failed(username: :unique)
+            validation_failed(username: :unique)
+          else
+            user = Core::Entities::User.new(username: username)
+            repo.save(user)
+            user_created(user.id)
           end
-          user = Core::Entities::User.new(username: username)
-          repo.save(user)
-          handler.user_created(user.id)
+        end
+
+        private
+
+        def self.validation_failed(errors)
+          Failure.new(errors)
+        end
+
+        def self.user_created(user_id)
+          Success.new(user_id)
         end
       end
     end
--- spec/core/use_cases/user/create_spec.rb
+++ spec/core/use_cases/user/create_spec.rb
@@ -5,34 +5,22 @@ require 'core/gateways/user/fake_user_repository'
 describe 'Create user' do
   CreateUser = Core::UseCases::User::Create
 
-  class HandlerSpy
-    attr_reader :spy_validation_errors, :spy_created_user_id
-
-    def validation_failed(errors)
-      @spy_validation_errors = errors
-    end
-
-    def user_created(id)
-      @spy_created_user_id = id
-    end
-  end
-
-  let(:handler) { HandlerSpy.new }
   let(:repo) { FakeUserRepository.new }
 
   it 'requires a username' do
-    CreateUser.execute(username: '', handler: handler, repo: repo)
-    expect(handler.spy_validation_errors).to include(username: :required)
+    result = CreateUser.execute(username: '', repo: repo)
+    expect(result).to eql CreateUser::Failure.new(username: :required)
   end
 
   it 'requires usernames to be unique' do
-    CreateUser.execute(username: 'valid name', handler: handler, repo: repo)
-    CreateUser.execute(username: 'valid name', handler: handler, repo: repo)
-    expect(handler.spy_validation_errors).to include(username: :unique)
+    CreateUser.execute(username: 'valid name', repo: repo)
+    result = CreateUser.execute(username: 'valid name', repo: repo)
+    expect(result).to eql CreateUser::Failure.new(username: :unique)
   end
 
   it 'sends an id for the created user back to the handler' do
-    CreateUser.execute(username: 'valid name', handler: handler, repo: repo)
-    expect(handler.spy_created_user_id).to be
+    result = CreateUser.execute(username: 'valid name', repo: repo)
+    expect(result).to be_a CreateUser::Success
+    expect(result.created_user_id).to be_a String
   end
 end
