aacdb39aa2915ab9ad0ffe993313e9800534b9e4
Peter Winton <pwinton@indigobio.com>
2017-02-07 19:39:35 -0500

    wip

--- lib/core/use_cases/user/create.rb
+++ lib/core/use_cases/user/create.rb
@@ -8,7 +8,12 @@ module Core
         Success = Struct.new(:created_user_id)
         Failure = Struct.new(:validation_errors)
 
-        def self.execute(username:, env:, repo:, &block)
+        def initialize(&handler)
+          validate_handler(handler)
+          @handler = handler
+        end
+
+        def execute(username:, env:, repo:)
           result =
             if username.empty?
               validation_failed(username: :required)
@@ -19,16 +24,25 @@ module Core
               repo.save(user, env: env)
               user_created(user.id)
             end
-          StrictCaseMatcher.match(result, &block)
+          StrictCaseMatcher.match(result, &@handler)
+        end
+
+        # for testing only
+        def self.execute(**kws, &handler)
+          new(&handler).execute(**kws)
         end
 
         private
 
-        def self.validation_failed(errors)
+        def validate_handler(handler)
+
+        end
+
+        def validation_failed(errors)
           Failure.new(errors)
         end
 
-        def self.user_created(user_id)
+        def user_created(user_id)
           Success.new(user_id)
         end
       end
---
+++ lib/core/use_cases/user/create_many.rb
@@ -0,0 +1,34 @@
+require 'core/entities/user'
+require 'core/util/strict_case_matcher'
+require 'core/use_cases/user/create'
+
+module Core
+  module UseCases
+    module User
+      class CreateMany
+        Success = Struct.new(:created_user_ids)
+        Failure = Struct.new(:validation_errors)
+
+        Creator = Create.new do
+
+        def execute(usernames:, env:, repo:, &block)
+          results = usernames.map do |username|
+            Create.execute(username: username, env: env, repo: repo, &handle_created)
+          end
+          StrictCaseMatcher.match(aggregate_result(results), &block)
+        end
+
+        private
+
+        def aggregate_result(results)
+          successes, failures = results.partition { |r| r.is_a?(Success) }
+          if failures.any?
+            Failure.new(failures.flat_map(&:validation_errors))
+          else
+            Success.new(successes.map(&:created_user_id))
+          end
+        end
+      end
+    end
+  end
+end
