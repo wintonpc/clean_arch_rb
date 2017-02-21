c9fd066adb356424fa53435025ae23594174ef51
Peter Winton <pwinton@indigobio.com>
2017-02-21 16:17:44 -0500

    wip

--- core/spec/ports/protocol_spec.rb
+++ core/spec/ports/protocol_spec.rb
@@ -36,28 +36,6 @@ module CommonPredicates
   end
 end
 
-# protocol :repo do
-#   role :repo do |r|
-#     r.save(user)
-#   end
-#
-#   role :client do |c|
-#     c.save_succeeded
-#     c.save_failed
-#   end
-#
-#   interactions do
-#     seq do
-#       repo.save
-#       one_of do # constructs like `one_of` help concisely describe execution path branching
-#         client.save_succeeded
-#         client.save_failed
-#       end
-#     end
-#   end
-# end
-
-
 # a protocol...
 protocol :create_user do
   # ... specifies two roles, each having an interface
@@ -83,24 +61,24 @@ protocol :create_user do
   end
 end
 
-# creates a module ...
-module Protocols
-  module CreateUser
-    module Interactor
-      def create_user(name)
-        raise 'not implemented'
-      end
-    end
-    module Client
-      def user_created(id)
-        raise 'not implemented'
-      end
-      def validation_failed(error)
-        raise 'not implemented'
-      end
-    end
-  end
-end
+# automatically creates a module ...
+# module Protocols
+#   module CreateUser
+#     module Interactor
+#       def create_user(name)
+#         raise 'not implemented'
+#       end
+#     end
+#     module Client
+#       def user_created(id)
+#         raise 'not implemented'
+#       end
+#       def validation_failed(error)
+#         raise 'not implemented'
+#       end
+#     end
+#   end
+# end
 
 module CreateUser
   # we specify which protocol role we interact with
@@ -156,9 +134,9 @@ class Port
 end
 
 describe CreateUser do
-  let(:subject) { instance_of(CreateUser) }
+  let(:subject) { instance_of(CreateUser) } # make and instantiate a class that includes the CreateUser module
   let(:rails) { spy(RailsCreateUser) }
-  let(:port) { Port.connect(create_user, rails) }
+  let(:port) { Port.connect(subject, rails) }
 
   it 'should do something' do
     port.create_user('valid name')
